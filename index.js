dff8a5943c4c9ea9a0595559ec5847b1689048ad
marissa.guevara717@gmail.com Orea <marissa.guevara717@gmail.com>
2018-05-18 18:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+=======
+var greetSpecialGuest = "Hello," + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = "Let's talk about" + `${topic}`;
+>>>>>>> 3eaff5bc5ce7a955c76a2bcde241f01ee81e1533
